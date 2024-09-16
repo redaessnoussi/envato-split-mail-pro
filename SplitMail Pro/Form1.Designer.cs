9110e5ea0c7d8f0e6aebd14fea3eef3eb2b7d0e9
Reda Essnoussi <71893153+redaessnoussi@users.noreply.github.com>
2024-09-16 18:12:13 +0100

    added packages and split emails by domain extension and bulk

--- SplitMail Pro/Form1.Designer.cs
+++ SplitMail Pro/Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtEmailList = new System.Windows.Forms.TextBox();
+            this.btnSplitEmails = new System.Windows.Forms.Button();
+            this.lstEmailGroups = new System.Windows.Forms.ListBox();
+            this.btnExportSelected = new System.Windows.Forms.Button();
+            this.btnExportAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmailList
+            // 
+            this.txtEmailList.Location = new System.Drawing.Point(12, 12);
+            this.txtEmailList.Multiline = true;
+            this.txtEmailList.Name = "txtEmailList";
+            this.txtEmailList.Size = new System.Drawing.Size(300, 200);
+            this.txtEmailList.TabIndex = 0;
+            // 
+            // btnSplitEmails
+            // 
+            this.btnSplitEmails.Location = new System.Drawing.Point(12, 218);
+            this.btnSplitEmails.Name = "btnSplitEmails";
+            this.btnSplitEmails.Size = new System.Drawing.Size(300, 23);
+            this.btnSplitEmails.TabIndex = 1;
+            this.btnSplitEmails.Text = "Split Emails";
+            this.btnSplitEmails.UseVisualStyleBackColor = true;
+            this.btnSplitEmails.Click += new System.EventHandler(this.btnSplitEmails_Click);
+            // 
+            // lstEmailGroups
+            // 
+            this.lstEmailGroups.FormattingEnabled = true;
+            this.lstEmailGroups.Location = new System.Drawing.Point(318, 12);
+            this.lstEmailGroups.Name = "lstEmailGroups";
+            this.lstEmailGroups.Size = new System.Drawing.Size(300, 200);
+            this.lstEmailGroups.TabIndex = 2;
+            // 
+            // btnExportSelected
+            // 
+            this.btnExportSelected.Location = new System.Drawing.Point(318, 218);
+            this.btnExportSelected.Name = "btnExportSelected";
+            this.btnExportSelected.Size = new System.Drawing.Size(145, 23);
+            this.btnExportSelected.TabIndex = 3;
+            this.btnExportSelected.Text = "Export Selected";
+            this.btnExportSelected.UseVisualStyleBackColor = true;
+            this.btnExportSelected.Click += new System.EventHandler(this.btnExportSelected_Click);
+            // 
+            // btnExportAll
+            // 
+            this.btnExportAll.Location = new System.Drawing.Point(473, 218);
+            this.btnExportAll.Name = "btnExportAll";
+            this.btnExportAll.Size = new System.Drawing.Size(145, 23);
+            this.btnExportAll.TabIndex = 4;
+            this.btnExportAll.Text = "Export All";
+            this.btnExportAll.UseVisualStyleBackColor = true;
+            this.btnExportAll.Click += new System.EventHandler(this.btnExportAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(634, 261);
+            this.Controls.Add(this.btnExportAll);
+            this.Controls.Add(this.btnExportSelected);
+            this.Controls.Add(this.lstEmailGroups);
+            this.Controls.Add(this.btnSplitEmails);
+            this.Controls.Add(this.txtEmailList);
+            this.Name = "Form1";
+            this.Text = "SplitMail Pro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtEmailList;
+        private System.Windows.Forms.Button btnSplitEmails;
+        private System.Windows.Forms.ListBox lstEmailGroups;
+        private System.Windows.Forms.Button btnExportSelected;
+        private System.Windows.Forms.Button btnExportAll;
     }
 }
-
--- SplitMail Pro/Form1.cs
+++ SplitMail Pro/Form1.cs
@@ -7,14 +7,121 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
 
 namespace SplitMail_Pro
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, List<string>> emailGroups;
+
         public Form1()
         {
             InitializeComponent();
+            emailGroups = new Dictionary<string, List<string>>();
+        }
+
+        private void btnSplitEmails_Click(object sender, EventArgs e)
+        {
+            SplitEmails();
+            DisplayEmailGroups();
+        }
+
+        private void SplitEmails()
+        {
+            emailGroups.Clear();
+            string[] emails = txtEmailList.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string email in emails)
+            {
+                string domain = email.Split('@')[1].ToLower();
+                string esp = GetESP(domain);
+
+                if (!emailGroups.ContainsKey(esp))
+                {
+                    emailGroups[esp] = new List<string>();
+                }
+                emailGroups[esp].Add(email);
+            }
+        }
+
+        private string GetESP(string domain)
+        {
+            if (domain.Contains("gmail")) return "Gmail";
+            if (domain.Contains("outlook") || domain.Contains("hotmail") || domain.Contains("live")) return "Outlook";
+            if (domain.Contains("yahoo")) return "Yahoo";
+            if (domain.Contains("aol")) return "AOL";
+            return "Other";
+        }
+
+        private void DisplayEmailGroups()
+        {
+            lstEmailGroups.Items.Clear();
+            foreach (var group in emailGroups)
+            {
+                lstEmailGroups.Items.Add($"{group.Key}: {group.Value.Count} email(s)");
+            }
+        }
+
+        private void btnExportSelected_Click(object sender, EventArgs e)
+        {
+            if (lstEmailGroups.SelectedItem == null) return;
+
+            string selectedESP = lstEmailGroups.SelectedItem.ToString().Split(':')[0];
+            ExportEmails(selectedESP);
+        }
+
+        private void btnExportAll_Click(object sender, EventArgs e)
+        {
+            ExportEmails();
+        }
+
+        private void ExportEmails(string esp = null)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Zip file (*.zip)|*.zip",
+                Title = "Save emails"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (var zipArchive = ZipFile.Open(saveFileDialog.FileName, ZipArchiveMode.Create))
+                {
+                    if (esp != null)
+                    {
+                        // Export specific ESP
+                        if (emailGroups.ContainsKey(esp))
+                        {
+                            var entry = zipArchive.CreateEntry($"{esp}.txt");
+                            using (var writer = new StreamWriter(entry.Open()))
+                            {
+                                foreach (var email in emailGroups[esp])
+                                {
+                                    writer.WriteLine(email);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Export all
+                        foreach (var group in emailGroups)
+                        {
+                            var entry = zipArchive.CreateEntry($"{group.Key}.txt");
+                            using (var writer = new StreamWriter(entry.Open()))
+                            {
+                                foreach (var email in group.Value)
+                                {
+                                    writer.WriteLine(email);
+                                }
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("Emails exported successfully!", "Export Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
